2cda3e24c3a9e72657728c30116441d4a11f572f
bcm-ray_crazy <65066644+bcmRayCrazy-coder@users.noreply.github.com>
2022-08-27 09:16:57 +0800

    Skill base

---
+++ src/main/java/cn/jerrymc/skilrp/skills/Skill.java
@@ -0,0 +1,83 @@
+package cn.jerrymc.skilrp.skills;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitScheduler;
+
+public class Skill {
+    // Name
+    private String name;
+    // Custom Model Data that display texture in client
+    private int modelData;
+    // Cooldown level (0 when it isn't cooldown, minus 1 per tick while cooldown)
+    private float cooldown;
+    // Skill level
+    private int level;
+
+    // cooldown task id
+    private int cooldownTaskId;
+
+    private JavaPlugin owner;
+
+    public Skill(JavaPlugin owner, String name, int modelData, float cooldownTicks, int level) {
+        this.owner = owner;
+        this.name = name;
+        this.modelData = modelData;
+        this.cooldown = cooldownTicks;
+        this.level = level;
+    }
+
+    public void use() {
+    }
+
+    public void startCooldown() {
+        BukkitScheduler scheduler = owner.getServer().getScheduler();
+        setCooldownTaskId(scheduler.scheduleSyncRepeatingTask(owner, new Runnable() {
+            @Override
+            public void run() {
+                if (!isCooldown()) {
+                    stopCooldown();
+                    return;
+                }
+                setCooldown(getCooldown() - 1.0f);
+            }
+        }, 0, 1L));
+    }
+
+    public void stopCooldown() {
+        BukkitScheduler scheduler = owner.getServer().getScheduler();
+        scheduler.cancelTask(cooldownTaskId);
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public float getCooldown() {
+        return cooldown;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getModelData() {
+        return modelData;
+    }
+
+    public void setCooldownTaskId(int cooldownTaskId) {
+        this.cooldownTaskId = cooldownTaskId;
+    }
+
+    public boolean isCooldown() {
+        return cooldown > 0.0f;
+    }
+
+    public void setCooldown(float cooldown) {
+        this.cooldown = cooldown;
+    }
+}
